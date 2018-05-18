f7bbf12b490429a0b121f6e71e8a2fc43ee44981
vanja <alhemyos@gmail.com>
2018-05-18 17:00:53 +0200

    login

--- front-end/src/components/login/login.vue
+++ front-end/src/components/login/login.vue
@@ -14,10 +14,31 @@
 
     <form class="login-form" name="login">
 
-      <img class="logo" src="/static/images/logo/vertigo_logo_lines.png" />
+      <img src="/static/images/logo/vertigo_logo_white.png" />
 
-      <input class="input" type="text" name="username" placeholder="username" v-model="username" />
-      <input class="input" type="password" name="password" placeholder="password" v-model="password" />
+      <div class="input-wrap">
+
+        <input class="input username" type="text" name="username" placeholder="username" v-model="username" @keypress="toggleUsernameActive" @keyup="validateUsername" />
+
+        <div class="input-loader">
+
+          <div :class="{ 'load' : usernameActive, 'error' : usernameError }" class="loader-item"></div>
+          <div :class="{ 'load' : usernameActive, 'error' : usernameError }" class="loader-item"></div>
+          <div :class="{ 'load' : usernameActive, 'error' : usernameError }" class="loader-item"></div>
+
+        </div>
+
+        <input class="input password" type="password" name="password" placeholder="password" v-model="password" @keypress="togglePasswordActive" @keyup="validatePassword" />
+
+        <div class="input-loader">
+
+          <div :class="{ 'load' : passwordActive }" class="loader-item"></div>
+          <div :class="{ 'load' : passwordActive }" class="loader-item"></div>
+          <div :class="{ 'load' : passwordActive }" class="loader-item"></div>
+
+        </div>
+
+      </div>
 
     </form>
 
@@ -27,14 +48,95 @@
 
 <script>
 
+  import _ from 'lodash'
+
   export default {
 
     name: 'login',
 
     computed: {
-      username: { get() { return this.$store.state.login.username } },
-      password: { get() { return this.$store.state.login.username } },
+      username: {
+        get() { return this.$store.state.login.username },
+        set( value ) { this.$store.commit( 'updateUsername', value ) }
+      },
+      usernameActive: {
+        get() { return this.$store.state.login.usernameActive }
+      },
+      usernameSuccess: { get() { return this.$store.state.login.usernameSuccess } },
+      usernameError: { get() { return this.$store.state.login.usernameError } },
+      password: {
+        get() { return this.$store.state.login.password },
+        set( value ) { this.$store.commit( 'updatePassword', value ) }
+      },
+      passwordActive: {
+        get() { return this.$store.state.login.passwordActive }
+      },
+      passwordSuccess: { get() { return this.$store.state.login.passwordSuccess } },
+      passwordError: { get() { return this.$store.state.login.passwordError } },
       validation: { get() { return this.$store.state.login.validation } }
+    },
+
+    methods: {
+
+      validateUsername: _.debounce(function() {
+
+        this.$store.commit( 'updateUsernameActive', false )
+
+        // REGEX check | letters only
+        if ( !this.username.match(this.$store.state.regex.letters) )
+          {
+            this.username = ""
+            $('.username').prop('placeholder', 'No numbers !')
+            this.$store.commit( 'updateUsernameError', true )
+
+            setTimeout( () => {
+
+              $('.username').prop('placeholder', 'username')
+              this.$store.commit( 'updateUsernameError', false )
+
+            },3000)
+          }
+
+        else
+          {
+            this.$store.commit( 'updateUsernameSuccess', true )
+          }
+
+      },500),
+
+      validatePassword: _.debounce(function() {
+
+        this.$store.commit( 'updatePasswordActive', false )
+
+        if ( !this.password.match(this.$store.state.regex.alphanumeric) )
+          {
+            this.password = ""
+            $('.password').prop('placeholder', 'Aha ! A hacker !')
+            this.$store.commit( 'updatePasswordError', true )
+
+            setTimeout( () => {
+
+              $('.password').prop('placeholder', 'password')
+              this.$store.commit( 'updatePasswordError', false )
+
+            },3000)
+          }
+
+        else
+          {
+            this.$store.commit( 'updatePasswordSuccess', true )
+          }
+
+      },500),
+
+      toggleUsernameActive() {
+        this.$store.commit( 'updateUsernameActive', true )
+      },
+
+      togglePasswordActive() {
+        this.$store.commit( 'updatePasswordActive', true )
+      }
+
     }
 
   }
@@ -49,9 +151,10 @@
       width: 0vw;
       height: 0vw;
       opacity: 0;
+      border-color: rgba(0,0,0,0.3);
     }
 
-    50% {
+    20% {
       opacity: 1;
     }
 
@@ -59,8 +162,24 @@
       width: 150vw;
       height: 150vw;
       opacity: 0;
+      border-color: #FF312E;
     }
 
+  }
+
+  @keyframes loader {
+
+    0% {
+      transform: translateY( 0px );
+    }
+
+    50% {
+      transform: translateY( -10px );
+    }
+
+    100% {
+      transform: translateY( 0px );
+    }
   }
 
   .login {
@@ -68,19 +187,114 @@
     width: 100%;
     height: 100vh;
 
-    background-color: #171A1E;
+    position: relative;
+    margin: none;
+
+    display: grid;
+    grid-template-columns: 50% 50%;
+    grid-template-rows: 100vh;
+
+    background-color: #333138;
+  }
+
+  .login-form {
+
+    width: auto;
+    height: 150px;
+
+    position: absolute;
+    top: 50%;
+    left: 0px;
+
+    transform: translateY( -50% );
+
+    padding: 0px 40px;
+
+    display: grid;
+    grid-template-columns: 150px auto;
+    grid-template-rows: 150px;
+    grid-column-gap: 40px;
+  }
+
+  .login-form img {
+
+    padding: 10px;
+  }
+
+  .input-wrap {
+
+    padding: 20px 0px;
+
+    display: grid;
+    grid-template-columns: 300px 40px;
+    grid-template-rows: 40px 40px;
+    grid-row-gap: 30px;
+    grid-column-gap: 10px;
+  }
+
+  .input {
+
+    font-size: 14px;
+    font-weight: lighter;
+    color: #FFFFFA;
+
+    background-color: transparent;
+    border: none;
+    border-bottom: 4px solid #FFFFFA;
+  }
+
+  .input::placeholder {
+
+    color: rgba(255,255,255,0.3)
+  }
+
+  .input-loader {
+
+    display: grid;
+    grid-template-columns: 4px 4px 4px;
+    grid-template-rows: 4px;
+    justify-content: space-around;
+    align-content: flex-end;
+  }
+
+  .loader-item {
+
+    background-color: #FFFFFA;
+  }
+
+  .loader-item:nth-child(1) {
+    animation-delay: 0.2s;
+  }
+
+  .loader-item:nth-child(2) {
+    animation-delay: 0.4s;
+  }
+
+  .load {
+
+    animation: loader 0.6s linear infinite;
+  }
+
+  .success {
+
+    background-color: #C8E087;
+  }
+
+  .error {
+
+    background-color: #FF312E;
   }
 
   .circles {
 
-    width: 100%;
-    height: 100vh;
+    width: 0px;
+    height: 0px;
 
     position: absolute;
     top: 50%;
-    left: 50%;
+    left: 120px;
 
-    transform: translate( -50%,-50% );
+    transform: translateY( -50% );
   }
 
   .circle {
@@ -91,65 +305,30 @@
 
     transform: translate( -50%,-50% );
 
-    border: 1px solid #050607;
+    border: 1px solid rgba(0,0,0,0.3);
     border-radius: 100%;
 
     animation: pulse 5s linear infinite;
   }
 
   .circle:nth-child(2) {
+
     animation-delay: 1s;
   }
 
   .circle:nth-child(3) {
+
     animation-delay: 2s;
   }
 
   .circle:nth-child(4) {
+
     animation-delay: 3s;
   }
 
   .circle:nth-child(5) {
+
     animation-delay: 4s;
-  }
-
-  .login-form {
-
-    width: auto;
-    height: auto;
-
-    position: absolute;
-    top: 50%;
-    left: 50%;
-
-    display: grid;
-    grid-template-columns: 300px;
-    grid-template-rows: 150px 40px 40px;
-    grid-row-gap: 20px;
-
-    transform: translate( -50%,-50% );
-  }
-
-  .input {
-
-    font-size: 14px;
-    font-weight: lighter;
-    color: #FDFFFC;
-    text-align: center;
-
-    width: 100%;
-    height: 40px;
-
-    padding: 0px 40px;
-
-    border: none;
-    border-radius: 40px;
-    background-color: #1B1F23;
-  }
-
-  .input::placeholder {
-
-    color: rgba(255,255,255,0.3)
   }
 
 </style>
--- front-end/src/store/modules/login.js
+++ front-end/src/store/modules/login.js
@@ -4,20 +4,38 @@ export default {
   state: {
 
     username: "",
+    usernameActive: false,
+    usernameSuccess: false,
+    usernameError: false,
     password: "",
+    passwordActive: false,
+    passwordSuccess: false,
+    passwordError: false,
     validation: false
 
   },
 
   getters: {
     username(state) { return state.username },
+    usernameActive(state) { return state.usernameActive },
+    usernameSuccess(state) { return state.usernameSuccess },
+    usernameError(state) { return state.usernameError },
     password(state) { return state.password },
+    passwordActive(state) { return state.passwordActive },
+    passwordSuccess(state) { return state.passwordSuccess },
+    passwordError(state) { return state.passwordError },
     validation(state) { return state.validation }
   },
 
   mutations: {
     updateUsername( state, username ) { state.username = username },
+    updateUsernameActive( state, usernameActive ) { state.usernameActive = usernameActive },
+    updateUsernameSuccess( state, usernameSuccess ) { state.usernameSuccess = usernameSuccess },
+    updateUsernameError( state, usernameError ) { state.usernameError = usernameError },
     updatePassword( state, password ) { state.password = password },
+    updatePasswordActive( state, passwordActive ) { state.passwordActive = passwordActive },
+    updatePasswordSuccess( state, passwordSuccess ) { state.passwordSuccess = passwordSuccess },
+    updatePasswordError( state, passwordError ) { state.passwordError = passwordError },
     updateValidation( state, validation ) { state.validation = validation }
   },
 
